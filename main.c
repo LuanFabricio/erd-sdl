1d54658d70b6a2edf505cbffd59cc779f90634de
Luan Fabrício <luanlimaleite@hotmail.com>
2026-07-27 23:45:34 -0300

    feat: updates `main` to use new functions

--- main.c
+++ main.c
@@ -13,23 +13,10 @@
 
 int main(void)
 {
-	HashMap parsed_map = parse_from_file("./sample.erd");
-	for (size_t i = 0; i < parsed_map.size; i++) {
-		Node node = parsed_map.items[i];
-		char buffer[NODE_VALUE_BUFFER_LEN];
-		memset(buffer, 0, NODE_VALUE_BUFFER_LEN);
-		node_value_to_cstr(node, buffer);
-		log_format(stdout, LOG_LABEL_INFO,
-				"\n"
-				"key:%s\n"
-				"value_ptr: %p\n"
-				"value: %s\n"
-				"kind: %s(%i)\n",
-				node.key,
-				node.value, buffer,
-				node_kind_to_cstr(node.kind), node.kind);
-	}
+	HashMap parsed_map = parse_from_filename("./sample.erd");
+	hash_map_log(parsed_map, 0);
 
+#if 0
 	InitWindow(720, 480,  "ERD");
 	char buffer[0xff];
 
@@ -54,6 +41,7 @@ int main(void)
 	}
 
 	CloseWindow();
+#endif
 
 	return 0;
 }
