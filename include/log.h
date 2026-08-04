58a95e1f9abbf00a6d6a356a19a510179a91e19c
Luan Fabricio <luanlimaleite@hotmail.com>
2026-08-04 19:02:00 -0300

    feat(log): implements `log_assert` and `log_panic`

--- include/log.h
+++ include/log.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "stdio.h"
+#include <assert.h>
+#include <stdbool.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 typedef enum {
 	LOG_LABEL_INFO,
@@ -18,4 +20,15 @@ const char* log_label_cstr(const log_label_e label);
 	do {\
 		fprintf(file, "[%s|%s:%d]: ", log_label_cstr(label), __FILE__, __LINE__); \
 		fprintf(file, __VA_ARGS__); \
+	} while(0)
+
+#define log_assert(exp, msg)\
+	do {  \
+		assert((exp) && msg);\
+	} while(0)
+
+#define log_panic(...) \
+	do {\
+		log_format(stderr, LOG_LABEL_ERROR, __VA_ARGS__); \
+		assert(false); \
 	} while(0)
